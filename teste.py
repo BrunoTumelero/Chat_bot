886ce35d73b152fe4ecf09759c0f7223d0bd353f
BrunoTumelero <Bruno.Tumelero@outlook.com>
2021-12-01 10:06:35 -0300

    atualizaçao-02

--- teste.py
+++ teste.py
@@ -1,21 +1,23 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+import json
 
 
 pages = set()
 def getlinks(pagesURL):
     global pages
-    html = urlopen('https://pt.wikipedia.org/wiki/{}'.format(pagesURL))
+    html = 'https://pt.wikipedia.org/wiki/{}'.format(pagesURL)
     s = BeautifulSoup(html, 'html.parser')
-
+    print(html)
     try:
-        print(s.find(id= 'firstHeading'))
-        print(s.find(class_='mw-parser-output').find_all('p'))
-        print(s.find(class_='mw-parser-output').find_all('ul'))
+        #print(s.find(id= 'firstHeading'))
+        #x = str(s.find(class_='mw-parser-output').find_all('p')[0])
+        
+        x = s.find('div', class_= 'mw-parser-output').find('p')
+        print(x.text)
 
     except AttributeError:
         print('Esta pagina não existe')
 
     
-
-getlinks('Amigos')
+getlinks('Poema')
